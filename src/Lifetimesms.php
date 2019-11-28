df533199d374a1e3a00369b7af277c28b3c411d2
lifetimesms <myeishvar@gmail.com>
2019-11-28 11:50:29 +0500

    Text to speech API added

--- src/Lifetimesms.php
+++ src/Lifetimesms.php
@@ -530,6 +530,95 @@ class Lifetimesms
         return ['status' => false, 'response' => $response];
     }
 
+    public function createVoiceFromTextToSpeech($params = [])
+    {
+        if (empty($params)) {
+            # code...
+            return ['status' => false, 'response' => 'Missing some mandatory parameters'];
+        }
+
+        if (!isset($params['text']) || empty($params['text'])) {
+            # code...
+            return ['status' => false, 'response' => 'Missing some mandatory parameters'];
+        }
+
+        if (!isset($params['title']) || empty($params['title'])) {
+            # code...
+            return ['status' => false, 'response' => 'Missing some mandatory parameters'];
+        }
+
+        if (!is_string($params['text'])) {
+            # code...
+            return ['status' => false, 'response' => 'Invalid text'];
+        }
+
+        if (!is_string($params['title'])) {
+            # code...
+            return ['status' => false, 'response' => 'Invalid text'];
+        }
+
+        $api_token = config('lifetimesms.api_token');
+        $api_secret = config('lifetimesms.api_secret');
+
+        if (!$api_secret || !$api_token) {
+            # code...
+            return ['status' => false, 'response' => 'API credentials are missing'];
+        }
+
+        $client = new Client(['timeout' => 320]);
+        $recieve_response = null;
+
+        $query = [
+            [
+                'name' => 'api_token',
+                'contents' => $api_token,
+            ],
+            [
+                'name' => 'api_secret',
+                'contents' => $api_secret,
+            ],
+            [
+                'name' => 'text_to_speech',
+                'contents' => $params['text'],
+            ],
+            [
+                'name' => 'title',
+                'contents' => $params['title'],
+            ],
+        ];
+
+        $requests = function() use ($query) {
+
+            $uri = "http://Lifetimesms.com/voice-request";
+            $parameters = new MultipartStream($query);
+            yield 1 => new GuzzleRequest('POST', $uri, [], $parameters);
+        };
+
+        $pool = new Pool($client, $requests(1), [
+            'concurrency' => 1,
+            'fulfilled' => function ($response, $id) use (&$recieve_response) {
+
+                $response = $response->getBody()->getContents();
+                $recieve_response = json_decode($response, true);
+            },
+            'rejected' => function ($reason, $id) use (&$recieve_response) {
+                $recieve_response = $reason->getMessage();
+            },
+        ]);
+
+        $promise = $pool->promise();
+        $promise->wait();
+
+        $response = $recieve_response;
+
+        if (isset($response['status']) && $response['status'] == 1) {
+            # code...
+            return ['status' => true, 'response' => $response];
+        }
+
+        return ['status' => false, 'response' => $response];
+    }
+
     public static function makeSingleAPIGetRequest($uri)
     {
     	$client = new Client(['timeout' => 320, 'verify' => false]);
